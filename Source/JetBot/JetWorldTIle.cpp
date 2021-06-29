95a868e8377a9ab3db10c762c2b41e1922f2c045
jhm520 <jhm520@gmail.com>
2021-06-28 20:06:31 -0700

    Initial push

---
+++ Source/JetBot/JetWorldTIle.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "JetWorldTIle.h"
+
+// Sets default values
+AJetWorldTIle::AJetWorldTIle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AJetWorldTIle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AJetWorldTIle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
