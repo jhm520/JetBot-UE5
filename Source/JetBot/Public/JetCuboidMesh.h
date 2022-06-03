41f8d229aa840a7b28f4ad37cbb0bc5e23d57950
LAPTOP-1H1INA1J\jhm52 <jhm520@gmail.com>
2022-06-02 18:09:12 -0700

    Finished cleaning up all non static functions in JetCuboidMesh

--- Source/JetBot/Public/JetCuboidMesh.h
+++ Source/JetBot/Public/JetCuboidMesh.h
@@ -39,15 +39,6 @@ class JETBOT_API AJetCuboidMesh : public AJetProcMesh
 public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Mesh")
-		TArray<FVector> Vertices;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Mesh")
-		TArray<int32> Triangles;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Mesh")
-		TArray<FVector2D> UVs;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Mesh")
 	FVector Dimensions = FVector(1, 1, 1);;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Procedural Mesh")
@@ -57,26 +48,13 @@ public:
 	static FProcMeshData CreateCuboidData(const FVector& InDimensions, int32 InTileSize);
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Procedural Mesh")
-	TArray<FVector> CreateCuboidVertexArray_Old(const FVector& InDimensions, int32 InTileSize);
-
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Procedural Mesh")
 	static TArray<FVector> CreateCuboidVertexArray(const FVector& InDimensions, int32 InTileSize, TArray<FProcMeshFaceVertexMap>& OutFaceVertexMapArray);
 
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Procedural Mesh")
 	static TArray<FVector2D> CreateCuboidUVArray(const TArray<FVector>& InVertices, const FVector& InDimensions, int32 InTileSize);
 
-	TArray<FVector2D> CreateCuboidUVArray_Bottom(const TArray<FVector>& InVertices, const FVector& InDimensions, int32 InTileSize);
-
-	TArray<FVector2D> CreateCuboidUVArray_Left(const TArray<FVector>& InVertices, const FVector& InDimensions, int32 InTileSize);
-
-	TArray<FVector2D> CreateCuboidUVArray_Front(const TArray<FVector>& InVertices, const FVector& InDimensions, int32 InTileSize);
-
-
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Procedural Mesh")
 	static TArray<int32> CreateCuboidTriangleArray(const FProcMeshData& InProcMeshData, const TArray<FVector>& InVertices, const FVector& InDimensions, int32 InTileSize);
-
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Procedural Mesh")
-	TArray<int32> CreateCuboidFaceTriangleArray(TMap<FVector, int32> InVertexIndexMap, const TArray<FVector>& InVertices, const FVector& InDimensions, int32 InTileSize, int32 InFaceDepth);
 
 	static TArray<int32> CreateCuboidTriangleArray_Bottom(const FProcMeshData& InProcMeshData, const TArray<FVector>& InVertices, const FVector& InDimensions, int32 InTileSize);
 
@@ -89,30 +67,6 @@ public:
 	static TArray<int32> CreateCuboidTriangleArray_Front(const FProcMeshData& InProcMeshData, const TArray<FVector>& InVertices, const FVector& InDimensions, int32 InTileSize);
 
 	static TArray<int32> CreateCuboidTriangleArray_Back(const FProcMeshData& InProcMeshData, const TArray<FVector>& InVertices, const FVector& InDimensions, int32 InTileSize);
-
-	//InVertexLocation is a vector of the cartesian coordinates. Returns the index
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Procedural Mesh")
-	static int32 GetCuboidVertexIndex_Old(const TArray<FVector>& InVertices, const FVector& InVertexLocation, const FVector& InDimensions);
-
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Procedural Mesh")
-	int32 GetCuboidVertexIndex(const FVector& InVertexLocation);
-
-	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Procedural Mesh")
-	const FCuboidVertexArray& GetCuboidVertexIndices(const FVector& InVertexLocation);
-
-	UPROPERTY()
-	FCuboidVertexArray EmptyVertex = FCuboidVertexArray();
-
-	//a map from cartesian coordinates to vertex index
-	UPROPERTY(BlueprintReadOnly, Category = "Procedural Mesh")
-	TMap<FVector, int> VertexIndexMap;
-
-	//a map from cartesian coordinates to vertex indices, each indices array is ordered by (bottom/top, left/right, front/back)
-	UPROPERTY(BlueprintReadOnly, Category = "Procedural Mesh")
-	TMap<FVector, FCuboidVertexArray> VertexIndicesMap;
-
-	//Get the index of the 
-	int32 GetCuboidVertexSideIndex(const FCuboidVertexArray& InCuboidVertexArray, ECuboidSide InCuboidSide);
 
 protected:
 	virtual void BeginPlay() override;
