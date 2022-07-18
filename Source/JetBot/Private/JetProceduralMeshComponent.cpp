c6ad1890421d6fe0bf0ce5cfc077083be76bd65a
LAPTOP-1H1INA1J\jhm52 <jhm520@gmail.com>
2022-07-18 12:29:34 -0700

    Made progress on JetProceduralmeshComponent

--- Source/JetBot/Private/JetProceduralMeshComponent.cpp
+++ Source/JetBot/Private/JetProceduralMeshComponent.cpp
@@ -5,7 +5,104 @@
 
 
 
+void UJetProceduralMeshComponent::JetFinishPhysicsAsyncCook(bool bSuccess, UBodySetup* FinishedBodySetup)
+{
+	TArray<UBodySetup*> NewQueue;
+	NewQueue.Reserve(JetAsyncBodySetupQueue.Num());
+
+	int32 FoundIdx;
+	if (JetAsyncBodySetupQueue.Find(FinishedBodySetup, FoundIdx))
+	{
+		if (bSuccess)
+		{
+			//The new body was found in the array meaning it's newer so use it
+			ProcMeshBodySetup = FinishedBodySetup;
+			RecreatePhysicsState();
+
+			//remove any async body setups that were requested before this one
+			for (int32 AsyncIdx = FoundIdx + 1; AsyncIdx < JetAsyncBodySetupQueue.Num(); ++AsyncIdx)
+			{
+				NewQueue.Add(JetAsyncBodySetupQueue[AsyncIdx]);
+			}
+
+			JetAsyncBodySetupQueue = NewQueue;
+		}
+		else
+		{
+			JetAsyncBodySetupQueue.RemoveAt(FoundIdx);
+		}
+	}
+}
+
+void UJetProceduralMeshComponent::JetCreateProcMeshBodySetup()
+{
+	if (ProcMeshBodySetup == nullptr)
+	{
+		ProcMeshBodySetup = JetCreateBodySetupHelper();
+	}
+}
+
 //TArray<TObjectPtr<UBodySetup>> UProceduralMeshComponent::GetAsyncBodySetupQueue()
 //{
 //	return AsyncBodySetupQueue;
 //}
+void UJetProceduralMeshComponent::JetUpdateCollision()
+{
+	//SCOPE_CYCLE_COUNTER(STAT_ProcMesh_UpdateCollision);
+
+	UWorld* World = GetWorld();
+	const bool bUseAsyncCook = World && World->IsGameWorld() && bUseAsyncCooking;
+
+	if (bUseAsyncCook)
+	{
+		// Abort all previous ones still standing
+		for (UBodySetup* OldBody : JetAsyncBodySetupQueue)
+		{
+			OldBody->AbortPhysicsMeshAsyncCreation();
+		}
+
+		JetAsyncBodySetupQueue.Add(JetCreateBodySetupHelper());
+	}
+	else
+	{
+		JetAsyncBodySetupQueue.Empty();	//If for some reason we modified the async at runtime, just clear any pending async body setups
+		JetCreateProcMeshBodySetup();
+	}
+
+	UBodySetup* UseBodySetup = bUseAsyncCook ? JetAsyncBodySetupQueue.Last() : ProcMeshBodySetup;
+
+	// Fill in simple collision convex elements
+	UseBodySetup->AggGeom.ConvexElems = JetCollisionConvexElems;
+
+	// Set trace flag
+	UseBodySetup->CollisionTraceFlag = bUseComplexAsSimpleCollision ? CTF_UseComplexAsSimple : CTF_UseDefault;
+
+	if (bUseAsyncCook)
+	{
+		UseBodySetup->CreatePhysicsMeshesAsync(FOnAsyncPhysicsCookFinished::CreateUObject(this, &UJetProceduralMeshComponent::JetFinishPhysicsAsyncCook, UseBodySetup));
+	}
+	else
+	{
+		// New GUID as collision has changed
+		UseBodySetup->BodySetupGuid = FGuid::NewGuid();
+		// Also we want cooked data for this
+		UseBodySetup->bHasCookedCollisionData = true;
+		UseBodySetup->InvalidatePhysicsData();
+		UseBodySetup->CreatePhysicsMeshes();
+		RecreatePhysicsState();
+	}
+}
+
+UBodySetup* UJetProceduralMeshComponent::JetCreateBodySetupHelper()
+{
+
+	// The body setup in a template needs to be public since the property is Tnstanced and thus is the archetype of the instance meaning there is a direct reference
+	UBodySetup* NewBodySetup = NewObject<UBodySetup>(this, NAME_None, (IsTemplate() ? RF_Public | RF_ArchetypeObject : RF_NoFlags));
+	NewBodySetup->BodySetupGuid = FGuid::NewGuid();
+
+	NewBodySetup->bGenerateMirroredCollision = false;
+	NewBodySetup->bDoubleSidedGeometry = true;
+	NewBodySetup->CollisionTraceFlag = bUseComplexAsSimpleCollision ? CTF_UseComplexAsSimple : CTF_UseDefault;
+
+	return NewBodySetup;
+}
--- Source/JetBot/Public/JetProceduralMeshComponent.h
+++ Source/JetBot/Public/JetProceduralMeshComponent.h
@@ -16,4 +16,26 @@ class JETBOT_API UJetProceduralMeshComponent : public UProceduralMeshComponent
 	
 	/*UFUNCTION()
 	TArray<UBodySetup*> GetAsyncBodySetupQueue();*/
+
+public:
+
+	/** Once async physics cook is done, create needed state */
+	void JetFinishPhysicsAsyncCook(bool bSuccess, UBodySetup* FinishedBodySetup);
+
+	/** Convex shapes used for simple collision */
+	UPROPERTY()
+	TArray<FKConvexElem> JetCollisionConvexElems;
+
+	/** Queue for async body setups that are being cooked */
+	UPROPERTY(transient)
+	TArray<TObjectPtr<UBodySetup>> JetAsyncBodySetupQueue;
+
+	/** Ensure ProcMeshBodySetup is allocated and configured */
+	void JetCreateProcMeshBodySetup();
+
+	/** Mark collision data as dirty, and re-create on instance if necessary */
+	void JetUpdateCollision();
+
+	/** Helper to create new body setup objects */
+	UBodySetup* JetCreateBodySetupHelper();
 };
